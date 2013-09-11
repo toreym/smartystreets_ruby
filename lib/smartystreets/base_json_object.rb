a0dd47091f06dfec4b758a81483ce8b0513f07b9
Peter Edge <peter@centzy.com>
2013-09-11 19:42:51 -0400

    commit

--- lib/smartystreets/base_json_object.rb
+++ lib/smartystreets/base_json_object.rb
@@ -17,49 +17,49 @@ module SmartyStreets
     private
 
     def get_optional_string(hash, key)
-      hash.has_key?(key) && !hash[key].empty? ? check_type(hash[key], String) : nil
+      hash[key] && !hash[key].empty? ? check_type(hash[key], String) : nil
     end
 
     def get_required_string(hash, key)
-      check_argument(hash.has_key?(key))
+      check_argument(hash[key])
       check_argument(!hash[key].empty?)
       check_type(hash[key], String)
     end
 
     def get_optional_fixnum(hash, key)
-      hash.has_key?(key) ? check_type(hash[key], Fixnum) : nil
+      hash[key] ? check_type(hash[key], Fixnum) : nil
     end
 
     def get_required_fixnum(hash, key)
-      check_argument(hash.has_key?(key))
+      check_argument(hash[key])
       check_type(hash[key], Fixnum)
     end
 
     def get_optional_float(hash, key)
-      hash.has_key?(key) ? check_type(hash[key], Float) : nil
+      hash[key] ? check_type(hash[key], Float) : nil
     end
 
     def get_required_float(hash, key)
-      check_argument(hash.has_key?(key))
+      check_argument(hash[key])
       check_type(hash[key], Float)
     end
 
     def get_optional_hash(hash, key)
-      hash.has_key?(key) && !hash[key].empty? ? check_type(hash[key], Hash) : nil
+      hash[key] && !hash[key].empty? ? check_type(hash[key], Hash) : nil
     end
 
     def get_required_hash(hash, key)
-      check_argument(hash.has_key?(key))
+      check_argument(hash[key])
       check_argument(!hash[key].empty?)
       check_type(hash[key], Hash)
     end
 
     def get_optional_array(hash, key)
-      hash.has_key?(key) && !hash[key].empty? ? check_type(hash[key], Array) : nil
+      hash[key] && !hash[key].empty? ? check_type(hash[key], Array) : nil
     end
 
     def get_required_array(hash, key)
-      check_argument(hash.has_key?(key))
+      check_argument(hash[key])
       check_argument(!hash[key].empty?)
       check_type(hash[key], Array)
     end
--- lib/smartystreets/version.rb
+++ lib/smartystreets/version.rb
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
 
 module SmartyStreets
-  GEM_VERSION = '0.0.4'
+  GEM_VERSION = '0.0.5'
 end
